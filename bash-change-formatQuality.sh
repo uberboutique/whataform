4d3a1ebc134decb20be270a9d7b8485b94883061
uberboutique <118627192+uberboutique@users.noreply.github.com>
2023-05-17 11:16:24 -0400

    Ajustes en la fotos

--- bash-change-formatQuality.sh
+++ bash-change-formatQuality.sh
@@ -1,3 +1,3 @@
-for f in $(find . -name '*.jpg' -or -name '*.jpeg'); do sips --resampleHeight 650 $f; done
-for f in $(find . -name '*.jpg' -or -name '*.jpeg'); do sips -s formatOptions normal $f; done
-for f in $(find . -name '*.jpg' -or -name '*.jpeg'); do sips -c 650 650 --padColor FFFFFF $f; done
+for f in $(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.JPG' -or -name '*.JPEG'); do sips --resampleHeight 650 $f; done
+for f in $(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.JPG' -or -name '*.JPEG'); do sips -s formatOptions normal $f; done
+for f in $(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.JPG' -or -name '*.JPEG'); do sips -c 650 650 --padColor FFFFFF $f; done
